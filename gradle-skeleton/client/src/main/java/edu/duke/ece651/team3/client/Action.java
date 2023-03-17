b19b35bfc2f745079119c84b1c9f93e153b17f4f
Luxin Zhang <67583508+LuxinZhang902@users.noreply.github.com>
2023-03-17 18:13:05 -0400

    Action and RuleCheck class

---
+++ gradle-skeleton/client/src/main/java/edu/duke/ece651/team3/client/Action.java
@@ -0,0 +1,7 @@
+package edu.duke.ece651.team3.client;
+
+import java.io.Serializable;
+
+public class Action implements Serializable {
+    public Action(){}
+}
---
+++ gradle-skeleton/shared/src/main/java/edu/duke/ece651/team3/shared/RuleChecker.java
@@ -0,0 +1,39 @@
+package edu.duke.ece651.team3.shared;
+
+public abstract class RuleChecker {
+
+    /**
+     * This method checks whether the source territory is valid
+     * @return The string contains the information
+     */
+    public String checkSrc(){
+        String info = null;
+        return info;
+    }
+    /**
+     * This method checks whether the Destination territory is valid
+     * @return The string contains the information
+     */
+    public String checkDes(){
+        String info = null;
+        return info;
+    }
+
+    /**
+     * This method checks whether the check the Unit is still valid
+     * @return The string contains the information
+     */
+    public String checkUnits(){
+        String info = null;
+        return info;
+    }
+
+    /**
+     * This method checks whether the path from src to des is valid
+     * @return The string contains the information
+     */
+    public String checkPath(){
+        String info = null;
+        return info;
+    }
+}
