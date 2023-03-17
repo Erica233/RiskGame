12d96bc99cef15346a914b9177a9d66e473df028
Fangting Ma <fangtingm@student.unimelb.edu.au>
2023-03-17 13:52:49 -0400

    json

---
+++ gradle-skeleton/server/src/main/java/edu/duke/ece651/team3/server/MapInitializer.java
@@ -0,0 +1,13 @@
+package edu.duke.ece651.team3.server;
+
+import org.json.simple.JSONObject;
+
+public class MapInitializer {
+    MapInitializer() {
+
+    }
+    public void parseJsonMap(String filename) {
+
+        JSONObject obj1 = new JSONObject();
+    }
+}
