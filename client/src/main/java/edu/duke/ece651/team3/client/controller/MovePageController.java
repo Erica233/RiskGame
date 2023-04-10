81372d60adce827b5e690b8b06658110fa662b89
Luxin Zhang <67583508+LuxinZhang902@users.noreply.github.com>
2023-04-09 20:35:47 -0400

    MovePageController

--- client/src/main/java/edu/duke/ece651/team3/client/controller/MovePageController.java
+++ client/src/main/java/edu/duke/ece651/team3/client/controller/MovePageController.java
@@ -1,4 +1,44 @@
 package edu.duke.ece651.team3.client.controller;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 
-public class MovePageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MovePageController implements Initializable {
+    @FXML
+    private ChoiceBox<?> LV0_choice;
+    @FXML
+    private ChoiceBox<?> LV1_choice;
+    @FXML
+    private ChoiceBox<?> LV2_choice;
+    @FXML
+    private ChoiceBox<?> LV3_choice;
+    @FXML
+    private ChoiceBox<?> LV4_choice;
+    @FXML
+    private ChoiceBox<?> LV5_choice;
+    @FXML
+    private ChoiceBox<?> LV6_choice;
+    @FXML
+    private ChoiceBox<?> choice_source;
+    @FXML
+    private ChoiceBox<?> choice_Dest;
+    @FXML
+    private Label playerColor;
+    @FXML
+    private Button nextButton;
+
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        LV0_choice.getItems().addAll("1", "2", "3");
+    }
+
+
+
 }
